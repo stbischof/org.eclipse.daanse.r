73983c5cd0fa44cdd83cf2003eab24e1f3bf5ba5
Stefan Bischof <stbischof@bipolis.org>
2026-02-10 23:33:56 +0100

    tests setup + ci workflow-doc
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,9 @@
+#' Say hello
+#'
+#' Returns a greeting string.
+#'
+#' @param name Character scalar. Name to greet.
+#' @return A character scalar greeting.
 #' @export
 hello <- function(name = "Eclipse") {
   paste0("Hello, ", name, "!")
